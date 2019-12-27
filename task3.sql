68fc5d952a28000c729c6784d34b0d06aa37ac9f
Deepan Raj R <deepanraj151997@gmail.com>
2019-12-27 14:51:45 +0530

    Update task3.sql

--- task3.sql
+++ task3.sql
@@ -18,3 +18,30 @@ insert into books(book_name,book_id,authour_name,category,publisher,price,versio
 insert into books(book_name,book_id,authour_name,category,publisher,price,version,active)values('life of the pie',20,'sugumar','documentary','kumar',450,2,'yes');
 insert into books(book_name,book_id,authour_name,category,publisher,price,version,active)values('pele',22,'sivika','documentary','mohan',450,1,'no');
 insert into books(book_name,book_id,authour_name,category,publisher,price,version,active)values('romance',33,'suri','romantic','dilp',600,4,'no');
+
+
+create table orders(order_id number(20)not null,
+user_name varchar2(20)not null,
+book_id number(20)not null,
+order_date timestamp default systimestamp,
+delivered_date timestamp,
+total_amount number(20) not null,
+quantity number(20)not null,
+order_status varchar2(20),
+                    
+constraint order_id_pk primary key(order_id),
+constraint book_id_fk foreign key(book_id) references books(book_id),
+constraint total_amount_ck check(total_amount>0),
+constraint quantity_qy check(quantity>=1));
+
+insert into orders(order_id,user_name,book_id,total_amount,quantity,order_status)
+values(123,'sandy',22,650,2,'yes');
+
+insert into orders(order_id,user_name,book_id,total_amount,quantity,order_status,delivered_date)
+values(124,'Ajamal',12,333,3,'yes','28-dec-2019');
+
+insert into orders(order_id,user_name,book_id,total_amount,quantity,order_status,delivered_date)
+values(144,'kumar',20,12,1,'yes','28-dec-2019');
+
+--insert into orders(order_id,user_name,book_id,total_amount,quantity,order_status,delivered_date)
+--values(220,'Gowtham',980,888,2,'yes','28-dec-2019');
