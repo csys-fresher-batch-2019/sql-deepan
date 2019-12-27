640b0007d394a2f928fee8cba76111fbcfba00ca
Deepan Raj R <deepanraj151997@gmail.com>
2019-12-27 17:29:50 +0530

    Update task3.sql

--- task3.sql
+++ task3.sql
@@ -45,3 +45,35 @@ values(144,'kumar',20,12,1,'yes','28-dec-2019');
 
 --insert into orders(order_id,user_name,book_id,total_amount,quantity,order_status,delivered_date)
 --values(220,'Gowtham',980,888,2,'yes','28-dec-2019');
+                             
+                             
+ 
+create table book_stock(stock_id number(10),
+book_id number(20),
+quantity number(20),
+                   
+constraint book_fk foreign key (book_id) references books(book_id));
+
+insert into book_stock(stock_id,book_id,quantity)values(1000,11,200);
+insert into book_stock(stock_id,book_id,quantity)values(1002,12,218);
+insert into book_stock(stock_id,book_id,quantity)values(1021,22,39);
+insert into book_stock(stock_id,book_id,quantity)values(1021,22,39);
+
+create table order_item(item_id number(10),
+order_id number(20),
+book_id number(20),
+quantity number(20),
+status varchar2(20),
+                   
+constraint order_id foreign key (order_id) references orders(order_id),
+constraint book_id foreign key (book_id) references books(book_id));
+
+insert into order_item(item_id,order_id,book_id,quantity,status)values(1,123,11,350,'yes');
+insert into order_item(item_id,order_id,book_id,quantity,status)values(2,124,12,450,'no');
+insert into order_item(item_id,order_id,book_id,quantity,status)values(3,144,20,600,'yes');
+
+select quantity from book_stock where book_id=12;
+select sum(quantity) from order_item where book_id=12;
+select book_name,(select sum(quantity) from order_item where book_id=b.book_id)from books b;
+select book_name,(select quantity from book_stock where book_id=b.book_id)from books b;
+                                                          
